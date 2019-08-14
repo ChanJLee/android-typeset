20def7ee727507e9024d3d74e1d11c1ade5493d3
jiacheng.li <jiacheng.li@shanbay.com>
2019-08-14 09:41:33 +0800

    rename break & add point

--- library/src/main/java/me/chan/typeset/core/Break.java
+++ library/src/main/java/me/chan/typeset/core/Break.java
@@ -1,11 +1,6 @@
 package me.chan.typeset.core;
 
 public class Break {
-	public int position = 0;
-	public float demerits = 0;
-	public float ratio = 0;
-	public int line = 0;
-	public int fitnessClazz = 0;
-	public Sum totals;
-	public Node prev;
+	public int position;
+	public float ratio;
 }
--- library/src/main/java/me/chan/typeset/core/Node.java
+++ library/src/main/java/me/chan/typeset/core/Node.java
@@ -1,11 +1,11 @@
 package me.chan.typeset.core;
 
 public class Node {
-	public Break data;
+	public Point data;
 	public Node prev;
 	public Node next;
 
-	public Node(Break data, Node prev, Node next) {
+	public Node(Point data, Node prev, Node next) {
 		this.data = data;
 		this.prev = prev;
 		this.next = next;
