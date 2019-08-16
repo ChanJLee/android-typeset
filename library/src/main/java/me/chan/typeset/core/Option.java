40f58a6ae43f7c255fc2ffa4fa2cdb17505aebe2
jiacheng.li <jiacheng.li@shanbay.com>
2019-08-16 11:29:42 +0800

    fix option hypen penalty

--- library/src/main/java/me/chan/typeset/core/Option.java
+++ library/src/main/java/me/chan/typeset/core/Option.java
@@ -3,11 +3,20 @@ package me.chan.typeset.core;
 import android.graphics.Paint;
 
 public class Option {
-	public float hyphenWidth = 0;
+	public int tolerance;
+	public int infinity = 1000;
+	public int minHyperLength = 4;
+
 	public float spaceWidth = 0;
 	public float spaceStretch = 0;
 	public float spaceShrink = 0;
-	public int tolerance;
+
+	public float hyphenWidth = 0;
+	public int hyphenPenalty = 100;
+
+	public float demeritsLine = 10;
+	public float demeritsFlagged = 100;
+	public float demeritsFitness = 3000;
 
 	public Option(Paint paint) {
 		reset(paint);
@@ -19,12 +28,4 @@ public class Option {
 		spaceStretch = (spaceWidth * 3) / 6;
 		spaceShrink = (spaceWidth * 3) / 9;
 	}
-
-	public int infinity = 1000;
-	public int hyphenPenalty = 100;
-	public int minHyperLength = 4;
-
-	public float demeritsLine = 100;
-	public float demeritsFlagged = 100;
-	public float demeritsFitness = 3000;
 }
