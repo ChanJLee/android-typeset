6d63a52e1079d6575ac12d3a7bea9d1550cc659c
chan <1355425625@qq.com>
2019-09-07 21:08:30 +0800

    remove old lib

--- app/src/main/java/me/chan/androidtex/MainActivity.java
+++ app/src/main/java/me/chan/androidtex/MainActivity.java
@@ -1,54 +1,16 @@
 package me.chan.androidtex;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.text.PrecomputedText;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
-import me.chan.typeset.TypesetView;
 
 public class MainActivity extends AppCompatActivity {
 
-	private TypesetView mTypesetView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final String content = getResources().getString(R.string.test);
-		mTypesetView = findViewById(R.id.view);
-		mTypesetView.setText(content);
-
-		final TextView textView = findViewById(R.id.text);
-		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					final PrecomputedText.Params params = new PrecomputedText.Params.Builder(textView.getPaint())
-							.setBreakStrategy(Layout.BREAK_STRATEGY_HIGH_QUALITY)
-							.build();
-					final PrecomputedText precomputedText = PrecomputedText.create(content, params);
-					textView.post(new Runnable() {
-						@Override
-						public void run() {
-							textView.setTextMetricsParams(params);
-							textView.setText(precomputedText);
-						}
-					});
-				}
-			}).start();
-		} else {
-			textView.setText(content);
-		}
-	}
-
-	@Override
-	protected void onDestroy() {
-		if (mTypesetView != null) {
-			mTypesetView.release();
-		}
-		super.onDestroy();
 	}
 }
--- library/src/main/java/me/chan/typeset/elements/Element.java
+++
@@ -1,9 +0,0 @@
-package me.chan.typeset.elements;
-
-public class Element {
-	public final float width;
-
-	public Element(float width) {
-		this.width = width;
-	}
-}
