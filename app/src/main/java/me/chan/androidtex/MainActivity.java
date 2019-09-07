a398e119c2f45440caa91db45045e0886d13112d
chan <1355425625@qq.com>
2019-09-07 23:07:08 +0800

    add test code

--- app/src/main/java/me/chan/androidtex/MainActivity.java
+++ app/src/main/java/me/chan/androidtex/MainActivity.java
@@ -1,16 +1,24 @@
 package me.chan.androidtex;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), TextureViewTestActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
