6095656d15b847798dca2d59753cc5c1ef7a0978
chan <1355425625@qq.com>
2019-09-25 23:06:46 +0800

    clean unit test code

--- te/src/test/java/me/chan/te/ExampleUnitTest.java
+++ te/src/test/java/me/chan/te/ExampleUnitTest.java
@@ -8,12 +8,10 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import me.chan.te.config.LineAttribute;
