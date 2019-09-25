84603d976564eae1edfc65db6091f0e29f5f2b59
chan <1355425625@qq.com>
2019-09-25 22:44:04 +0800

    add test code

--- te/src/main/java/me/chan/te/renderer/Adapter.java
+++ te/src/main/java/me/chan/te/renderer/Adapter.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
 import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.TypedValue;
