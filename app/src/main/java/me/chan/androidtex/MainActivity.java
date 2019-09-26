8428664059db4d13093193924b6ca2b30a259dd7
jiacheng.li <jiacheng.li@shanbay.com>
2019-09-26 14:17:19 +0800

    add test code

--- app/src/main/java/me/chan/androidtex/MainActivity.java
+++ app/src/main/java/me/chan/androidtex/MainActivity.java
@@ -3,6 +3,10 @@ package me.chan.androidtex;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.text.TextPaint;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 
 
@@ -20,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(intent);
 			}
 		});
+
+		TextPaint textPaint = new TextPaint();
+		textPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 18, getResources().getDisplayMetrics()));
+		Log.d("chan_debug", Layout.getDesiredWidth("-", textPaint) + "");
+		Log.d("chan_debug", Layout.getDesiredWidth(" ", textPaint) + "");
+		Log.d("chan_debug", Layout.getDesiredWidth("qwertyuioplkjhgfdsazxcvbnm-", textPaint) + "");
+		Log.d("chan_debug", Layout.getDesiredWidth("qwertyuioplkjhgfdsazxcvbnm", textPaint) + "");
 
 		findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
 			@Override
