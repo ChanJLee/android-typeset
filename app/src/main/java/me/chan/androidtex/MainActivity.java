100d6cb39a1750509222c899bf74386e52deaa72
jiacheng.li <jiacheng.li@shanbay.com>
2019-09-27 13:34:01 +0800

    add simple typesetter

--- app/src/main/java/me/chan/androidtex/MainActivity.java
+++ app/src/main/java/me/chan/androidtex/MainActivity.java
@@ -39,5 +39,13 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(intent);
 			}
 		});
+
+		findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), AndroidTestActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
