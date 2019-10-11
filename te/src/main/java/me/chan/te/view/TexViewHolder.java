b154d7caa445d73a26bab28d81dd00964a50b519
jiacheng.li <jiacheng.li@shanbay.com>
2019-10-11 16:20:53 +0800

    clean import

--- te/src/main/java/me/chan/te/view/TexViewHolder.java
+++ te/src/main/java/me/chan/te/view/TexViewHolder.java
@@ -4,8 +4,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-import me.chan.te.view.ParagraphView;
-
 class TexViewHolder extends RecyclerView.ViewHolder {
 
 	ParagraphView mParagraphView;
