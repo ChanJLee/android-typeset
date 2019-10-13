582d0f5f5ba8ecdcdfb5bc5d3b59f21112191297
chan <1355425625@qq.com>
2019-10-13 23:19:12 +0800

    save process

--- te/src/main/java/me/chan/te/typesetter/SimpleTypesetter.java
+++ te/src/main/java/me/chan/te/typesetter/SimpleTypesetter.java
@@ -6,25 +6,29 @@ import android.text.TextPaint;
 import java.util.ArrayList;
 import java.util.List;
 
-import me.chan.te.text.BreakStrategy;
 import me.chan.te.config.LineAttributes;
 import me.chan.te.config.Option;
 import me.chan.te.data.Box;
 import me.chan.te.data.Element;
+import me.chan.te.data.ElementFactory;
 import me.chan.te.data.Line;
 import me.chan.te.data.Paragraph;
 import me.chan.te.data.Penalty;
 import me.chan.te.data.Segment;
+import me.chan.te.log.Log;
+import me.chan.te.text.BreakStrategy;
 
 class SimpleTypesetter implements Typesetter {
 	private Option mOption;
 	private TextPaint mPaint;
 	private TextPaint mWorkPaint = new TextPaint();
 	private Box.Bound mBound = new Box.Bound();
+	private ElementFactory mElementFactory;
 
-	SimpleTypesetter(TextPaint paint, Option option) {
+	SimpleTypesetter(TextPaint paint, Option option, ElementFactory elementFactory) {
 		mOption = option;
 		mPaint = paint;
+		mElementFactory = elementFactory;
 	}
 
 	@NonNull
@@ -34,13 +38,11 @@ class SimpleTypesetter implements Typesetter {
 		// 一行尽可能的占满尽可能多的字符
 		// 如果如果只显示了一个并且还不足以完美显示，那么无脑折断
 		List<? extends Element> elements = segment.getElements();
-		int lineNumber = 0;
 		List<Line> lines = new ArrayList<>();
 		int size = elements.size();
 		for (int i = 0; i < size; ) {
-			float width = lineAttributes.get(lineNumber).getLineWidth();
+			float width = lineAttributes.get(lines.size()).getLineWidth();
 			i = typesetLine(width, lines, elements, i, breakStrategy);
-			++lineNumber;
 		}
 		paragraph.setLines(lines);
 		return paragraph;
@@ -48,60 +50,45 @@ class SimpleTypesetter implements Typesetter {
 
 	private int typesetLine(float width, List<Line> lines, List<? extends Element> elements,
 							int start, BreakStrategy breakStrategy) {
-		List<Box> boxes = new ArrayList<>();
 		int size = elements.size();
+
+		// skip none box
+		for (; start < size; ++start) {
+			if (elements.get(start) instanceof Box) {
+				break;
+			}
+		}
+
+		if (start >= size) {
+			return start;
+		}
+
+		List<Box> boxes = new ArrayList<>();
 		float lineHeight = 0f;
 		float spaceWidth = mOption.getSpaceWidth();
 		float currentLineWidth = 0f;
 
-		main:
-		for (; start < size; ++start) {
+		while (start < size) {
 			Element element = elements.get(start);
 			if (!(element instanceof Box)) {
+				++start;
 				continue;
 			}
 
 			mWorkPaint.set(mPaint);
 			Box box = (Box) element;
+			int next = mergeIf(elements, box, start + 1, currentLineWidth, width);
 			box.getBound(mWorkPaint, mBound);
 
-			while (currentLineWidth + mBound.getWidth() > width && (start + 1) < size) {
-				if (!(elements.get(start + 1) instanceof Penalty)) {
-					break;
-				}
-
-
-				Penalty penalty = (Penalty) elements.get(start + 1);
-				if (penalty.getPenalty() == -mOption.INFINITY) {
-					break main;
-				}
-
-				if (penalty.getPenalty() == mOption.INFINITY) {
-					break;
-				}
-
-				start += 2;
-				if (start >= size) {
-					break;
-				}
-
-				if (!(elements.get(start) instanceof Box)) {
-					break;
-				}
-
-				Box next = (Box) elements.get(start);
-				if (!box.canMerge(next)) {
-					break;
-				}
-
-				box.append(next);
+			// 如果超出当前的长度 那么直接结束
+			if (currentLineWidth + mBound.getWidth() > width) {
+				break;
 			}
 
+			start = next;
 			box.getBound(mWorkPaint, mBound);
-
 			boxes.add(box);
 			currentLineWidth += (mBound.getWidth() + spaceWidth);
-
 			if (lineHeight < mBound.getHeight()) {
 				lineHeight = mBound.getHeight();
 			}
@@ -109,11 +96,12 @@ class SimpleTypesetter implements Typesetter {
 
 		// 如果一行是空的，说明当前只能排一个，并且都显示不下
 		if (boxes.isEmpty()) {
-			mWorkPaint.set(mPaint);
-			handleSingleBoxLine(boxes, elements, start, width, mBound, mWorkPaint);
+			start = spiltIf(elements, start, boxes, mBound, width);
 			lineHeight = mBound.getHeight();
-		} else {
-			start = handleFullLoadLine(elements, start - 1, width, currentLineWidth, boxes);
+		}
+
+		if (boxes.isEmpty()) {
+			return start;
 		}
 
 		if (breakStrategy == BreakStrategy.BALANCED && boxes.size() > 1 && start != size) {
@@ -124,10 +112,53 @@ class SimpleTypesetter implements Typesetter {
 		return start;
 	}
 
-	private void handleSingleBoxLine(List<Box> boxes, List<? extends Element> elements,
-									 int start, float width, Box.Bound bound, TextPaint textPaint) {
-		mWorkPaint.set(mPaint);
+	private int mergeIf(List<? extends Element> elements, Box box,
+						int start, float currentLineWidth, float width) {
+		Box clone = null;
+		int prev = start;
+		for (; start < elements.size(); ++start) {
+			if (!(elements.get(start) instanceof Penalty)) {
+				break;
+			}
+
+			if (start + 1 < elements.size() &&
+					!(elements.get(start + 1) instanceof Box)) {
+				break;
+			}
+
+			Box next = (Box) elements.get(start + 1);
+			if (!next.canMerge(box)) {
+				return prev;
+			}
+
+			if (clone == null) {
+				clone = (Box) box.clone();
+			}
+
+			clone.append(next);
+			clone.getBound(mWorkPaint, mBound);
+			// 如果超出当前的长度 那么直接结束
+			if (currentLineWidth + mBound.getWidth() > width) {
+				return prev;
+			}
+
+			++start;
+		}
+
+		if (clone != null) {
+			box.copy(clone);
+		}
+
+		return start;
+	}
+
+	private int spiltIf(List<? extends Element> elements, int start, List<Box> boxes, Box.Bound bound, float width) {
+		if (start >= elements.size()) {
+			return start;
+		}
+
 		Box box = (Box) elements.get(start);
+		mWorkPaint.set(mPaint);
 		Box[] children = box.spilt(mWorkPaint, width);
 		if (children != null) {
 			children[0].setPenalty(true);
@@ -136,29 +167,9 @@ class SimpleTypesetter implements Typesetter {
 			box = children[0];
 		} else {
 			boxes.add(box);
-		}
-		box.getBound(textPaint, bound);
-	}
-
-	private int handleFullLoadLine(List<? extends Element> elements, int start, float width, float currentWidth, List<Box> boxes) {
-		int last = start + 1;
-		Element next;
-		if (last >= elements.size() ||
-				!((next = elements.get(last)) instanceof Penalty)) {
-			return start;
-		}
-
-		if (((Penalty) next).getPenalty() != mOption.INFINITY &&
-				currentWidth + mOption.getHyphenWidth() <= width) {
-
-			Box box = boxes.get(boxes.size() - 1);
-			box.setPenalty(true);
-			box.append("-");
 			++start;
-		} else {
-			boxes.remove(boxes.size() - 1);
 		}
-
+		box.getBound(mWorkPaint, bound);
 		return start;
 	}
 }
