30266c2647f1c5459046ceec245d9bd01d99a5d7
jiacheng.li <jiacheng.li@shanbay.com>
2019-10-21 18:09:42 +0800

    add test code

--- app/src/main/java/me/chan/androidtex/MainActivity.java
+++ app/src/main/java/me/chan/androidtex/MainActivity.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
+import java.io.UnsupportedEncodingException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +19,13 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		TextPaint textPaint = new TextPaint();
+		final TextPaint textPaint = new TextPaint();
 		textPaint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 18, getResources().getDisplayMetrics()));
 		Log.d("chan_debug", Layout.getDesiredWidth("-", textPaint) + "");
 		Log.d("chan_debug", Layout.getDesiredWidth(" ", textPaint) + "");
 		Log.d("chan_debug", Layout.getDesiredWidth("qwertyuioplkjhgfdsazxcvbnm-", textPaint) + "");
 		Log.d("chan_debug", Layout.getDesiredWidth("qwertyuioplkjhgfdsazxcvbnm", textPaint) + "");
+
 
 		findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -39,5 +42,36 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(intent);
 			}
 		});
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Log.d("chan_debug", "test lower");
+					test(95, 122, textPaint);
+					Log.d("chan_debug", "test upper");
+					test(65, 90, textPaint);
+					Log.d("chan_debug", "test chinese");
+					test(0x4E00, 0x9FA5, textPaint);
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
+	private void test(int codePointStart, int codePointEnd, TextPaint textPaint) throws UnsupportedEncodingException {
+		StringBuilder stringBuilder = new StringBuilder();
+		float prev = 0;
+		for (int code = codePointStart; code <= codePointEnd; ++code) {
+			String current = String.valueOf((char) code);
+			float currentLen = Layout.getDesiredWidth(current, textPaint);
+			stringBuilder.append(current);
+			float last = Layout.getDesiredWidth(stringBuilder.toString(), textPaint);
+			if (last - prev != currentLen) {
+				throw new RuntimeException("unexpected char: " + current);
+			}
+			prev = last;
+		}
 	}
 }
