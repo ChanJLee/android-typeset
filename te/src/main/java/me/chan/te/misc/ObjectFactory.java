a9535b61cd281fd4532b425d0cb797d6c40e6581
chan <1355425625@qq.com>
2019-10-23 21:41:37 +0800

    add measurer's doc

--- te/src/main/java/me/chan/te/misc/ObjectFactory.java
+++ te/src/main/java/me/chan/te/misc/ObjectFactory.java
@@ -6,11 +6,21 @@ import android.support.annotation.Nullable;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+/**
+ * 对象工厂，用于存储频繁创建与销毁的类对象
+ *
+ * @param <T> 存储类型
+ */
 public class ObjectFactory<T> {
 
 	private Queue<T> mQueue;
 	private int mBufferSize;
 
+	/**
+	 * 创建一个对象工厂
+	 *
+	 * @param bufferSize 当前缓存大小
+	 */
 	public ObjectFactory(int bufferSize) {
 		if (bufferSize <= 0) {
 			throw new IllegalArgumentException("buffer size must be large than 0");
@@ -20,15 +30,25 @@ public class ObjectFactory<T> {
 		mBufferSize = bufferSize;
 	}
 
+	/**
+	 * 获取一个对象
+	 *
+	 * @return 返回一个对象，当当前对象工厂没有对象存储时，返回null
+	 */
 	@Nullable
 	public synchronized T acquire() {
 		return mQueue.poll();
 	}
 
-	public synchronized boolean release(@NonNull T t) {
+	/**
+	 * 回收一个对象
+	 *
+	 * @param obj 对象
+	 */
+	public synchronized void release(@NonNull T obj) {
 		if (mQueue.size() >= mBufferSize) {
-			return false;
+			return;
 		}
-		return mQueue.offer(t);
+		mQueue.offer(obj);
 	}
 }
