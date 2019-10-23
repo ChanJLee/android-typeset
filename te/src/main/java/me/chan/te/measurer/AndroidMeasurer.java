78dc3392350524374b470556cf93c64dd7262a2f
chan <1355425625@qq.com>
2019-10-23 21:41:37 +0800

    add measurer's doc

--- te/src/main/java/me/chan/te/measurer/AndroidMeasurer.java
+++ te/src/main/java/me/chan/te/measurer/AndroidMeasurer.java
@@ -4,6 +4,9 @@ import android.graphics.Paint;
 import android.text.BoringLayout;
 import android.text.TextPaint;
 
+/**
+ * android的文本测量器
+ */
 public class AndroidMeasurer implements Measurer {
 
 	private TextPaint mTextPaint;
@@ -14,6 +17,11 @@ public class AndroidMeasurer implements Measurer {
 		refresh(textPaint);
 	}
 
+	/**
+	 * 刷新当前的text paint
+	 *
+	 * @param textPaint text paint
+	 */
 	public void refresh(TextPaint textPaint) {
 		mTextPaint = textPaint;
 		Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
@@ -23,6 +31,9 @@ public class AndroidMeasurer implements Measurer {
 
 	@Override
 	public float getDesiredWidth(CharSequence charSequence, int start, int end) {
+		// 不能使用 TextPaint getTextBounds
+		// vivo 手机使用这个方法慢的出奇
+		// BoringLayout 是用来测量单行文本的
 		return BoringLayout.getDesiredWidth(charSequence, start, end, mTextPaint);
 	}
 
--- te/src/main/java/me/chan/te/measurer/Measurer.java
+++ te/src/main/java/me/chan/te/measurer/Measurer.java
@@ -1,9 +1,27 @@
 package me.chan.te.measurer;
 
+/**
+ * 文字测量器
+ */
 public interface Measurer {
+	/**
+	 * @param charSequence 文本
+	 * @param start        文本开始下表
+	 * @param end          文本结束下标
+	 * @return 文本宽度
+	 */
 	float getDesiredWidth(CharSequence charSequence, int start, int end);
 
+	/**
+	 * @param charSequence 文本
+	 * @param start        文本开始下表
+	 * @param end          文本结束下标
+	 * @return 文本高度
+	 */
 	float getDesiredHeight(CharSequence charSequence, int start, int end);
 
+	/**
+	 * @return 每行建议间隔
+	 */
 	float getFontSpacing();
 }
