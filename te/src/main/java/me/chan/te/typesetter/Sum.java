4c473cc3271f84d32badf13cdf10714f4ac9ce03
jiacheng.li <jiacheng.li@shanbay.com>
2019-10-25 17:26:11 +0800

    add sum recycle

--- te/src/main/java/me/chan/te/typesetter/Sum.java
+++ te/src/main/java/me/chan/te/typesetter/Sum.java
@@ -1,16 +1,37 @@
 package me.chan.te.typesetter;
 
-public class Sum {
+import me.chan.te.data.Recyclable;
+import me.chan.te.misc.ObjectFactory;
+
+public class Sum implements Recyclable {
+	private static final ObjectFactory<Sum> POOL = new ObjectFactory<>(100);
+
 	public float width = 0;
 	public float stretch = 0;
 	public float shrink = 0;
 
-	public Sum(Sum other) {
-		this.width = other.width;
-		this.shrink = other.shrink;
-		this.stretch = other.stretch;
+	private Sum() {
 	}
 
-	public Sum() {
+	@Override
+	public void recycle() {
+		width = shrink = stretch = -1;
+		POOL.release(this);
+	}
+
+	public static Sum obtain() {
+		Sum sum = POOL.acquire();
+		if (sum != null) {
+			return sum;
+		}
+		return new Sum();
+	}
+
+	public static Sum obtain(Sum other) {
+		Sum sum = obtain();
+		sum.width = other.width;
+		sum.shrink = other.shrink;
+		sum.stretch = other.stretch;
+		return sum;
 	}
 }
