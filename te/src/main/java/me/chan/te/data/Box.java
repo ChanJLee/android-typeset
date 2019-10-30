8bffe884574da871dd1d449ab1f371a6c2ed24ee
jiacheng.li <jiacheng.li@shanbay.com>
2019-10-30 16:06:07 +0800

    opt performance

--- te/src/main/java/me/chan/te/data/Box.java
+++ te/src/main/java/me/chan/te/data/Box.java
@@ -14,21 +14,24 @@ import me.chan.te.annotations.Hidden;
  * 比如一个单词，一张图片
  */
 public abstract class Box implements Cloneable, Element {
-	private static final int FLAG_NONE = 0;
+	static final int FLAG_NONE = 0;
 	static final int FLAG_PENALTY = 2;
-
-	// TODO refactor
-	public static final int FLAG_SPILT = 1;
+	static final int FLAG_SPILT = 1;
 
 	// 检查 equals
 	protected float mWidth;
 	protected float mHeight;
-
 	private int mFlag = FLAG_NONE;
+	protected Object mExtra;
 
-	public Box(float width, float height) {
+	public Box(float width, float height, Object extra) {
 		mWidth = width;
 		mHeight = height;
+		mExtra = extra;
+	}
+
+	public Object getExtra() {
+		return mExtra;
 	}
 
 	public float getWidth() {
@@ -39,24 +42,7 @@ public abstract class Box implements Cloneable, Element {
 		return mHeight;
 	}
 
-	@CallSuper
-	public void copy(@NonNull Box other) {
-		this.mFlag = other.mFlag;
-		this.mWidth = other.mWidth;
-		this.mHeight = other.mHeight;
-	}
-
 	public abstract Object clone();
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Box box = (Box) o;
-		return Float.compare(box.mWidth, mWidth) == 0 &&
-				Float.compare(box.mHeight, mHeight) == 0 &&
-				mFlag == box.mFlag;
-	}
 
 	@Hidden
 	public abstract void append(Box other);
@@ -91,6 +77,27 @@ public abstract class Box implements Cloneable, Element {
 
 	public abstract String toString();
 
+	public abstract boolean canSpilt();
+
 	@Nullable
-	public abstract Box[] spilt(float limitWidth);
+	public abstract Box spilt(float limitWidth);
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Box box = (Box) o;
+		return Float.compare(box.mWidth, mWidth) == 0 &&
+				Float.compare(box.mHeight, mHeight) == 0 &&
+				mFlag == box.mFlag &&
+				mExtra == box.mExtra;
+	}
+
+	@CallSuper
+	public void copy(@NonNull Box other) {
+		this.mFlag = other.mFlag;
+		this.mWidth = other.mWidth;
+		this.mHeight = other.mHeight;
+		this.mExtra = other.mExtra;
+	}
 }
