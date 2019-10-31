d6e82849322dd64de588c2ac202f16f5377ea1b4
jiacheng.li <jiacheng.li@shanbay.com>
2019-10-31 18:05:36 +0800

    add appearnce

--- te/src/main/java/me/chan/te/text/Background.java
+++ te/src/main/java/me/chan/te/text/Background.java
@@ -1,15 +1,66 @@
 package me.chan.te.text;
 
+import android.graphics.Canvas;
+import android.text.TextPaint;
+
+import me.chan.te.misc.ObjectFactory;
+import me.chan.te.misc.Recyclable;
+
 /**
  * 背景
  */
-public interface Background {
+public class Background implements Appearance, Recyclable {
+	private static final ObjectFactory<Background> POOL = new ObjectFactory<>(512);
 
-	/**
-	 * 检查样式是否冲突
-	 *
-	 * @param other 另外一个background
-	 * @return 是否和Other元素style冲突，如果不冲突，表示其对应的Box可以被合并
-	 */
-	boolean isConflict(Background other);
+	private int mColor;
+
+	private Background(int color) {
+		mColor = color;
+	}
+
+	public int getColor() {
+		return mColor;
+	}
+
+	public void setColor(int color) {
+		mColor = color;
+	}
+
+	@Override
+	public boolean isConflict(Appearance other) {
+		return !equals(other);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Background that = (Background) o;
+		return mColor == that.mColor;
+	}
+
+	@Override
+	public void draw(Canvas canvas, TextPaint textPaint, float left, float top, float right, float bottom) {
+		textPaint.setColor(mColor);
+		canvas.drawRect(left, top, right, bottom, textPaint);
+	}
+
+	@Override
+	public void recycle() {
+		mColor = -1;
+		POOL.release(this);
+	}
+
+	public static Background obtain(int color) {
+		Background background = POOL.acquire();
+		if (background == null) {
+			return new Background(color);
+		}
+		background.mColor = color;
+		return background;
+	}
+
+	public static void clean() {
+		POOL.clean();
+	}
 }
