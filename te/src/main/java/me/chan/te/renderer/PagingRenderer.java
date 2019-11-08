c28e6d19536fbf15867df3e55c96755218e26839
jiacheng.li <jiacheng.li@shanbay.com>
2019-11-08 18:09:46 +0800

    add sliding renderer

---
+++ te/src/main/java/me/chan/te/renderer/PagingRenderer.java
@@ -0,0 +1,32 @@
+package me.chan.te.renderer;
+
+import android.view.ViewGroup;
+
+import me.chan.te.text.Document;
+
+public class PagingRenderer extends Renderer {
+
+	public PagingRenderer(ViewGroup viewGroup, RenderOption renderOption) {
+		super(viewGroup.getContext(), renderOption);
+	}
+
+	@Override
+	protected void onClear() {
+
+	}
+
+	@Override
+	protected void onRenderer(Document document) {
+
+	}
+
+	@Override
+	protected void onError(Throwable throwable) {
+
+	}
+
+	@Override
+	protected void onRefresh(RenderOption renderOption) {
+
+	}
+}
--- te/src/main/java/me/chan/te/renderer/TeView.java
+++ te/src/main/java/me/chan/te/renderer/TeView.java
@@ -2,16 +2,20 @@ package me.chan.te.renderer;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.Typeface;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.PagerSnapHelper;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.TypedValue;
-import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
 
 import me.chan.te.R;
 import me.chan.te.parser.Parser;
@@ -22,13 +26,14 @@ public class TeView extends FrameLayout {
 
 	private static final int BREAK_STRATEGY_SIMPLE = 1;
 	private static final int BREAK_STRATEGY_BALANCE = 2;
+	private static final int MODE_PAGING = 1;
+	private static final int MODE_SLIDING = 2;
+	private static final int DEFAULT_TEXT_SIZE = 18;
+	private static final int DEFAULT_LINE_SPACE = 12;
+	private static final int DEFAULT_SEGMENT_SPACE = 28;
+	private static Map<String, WeakReference<Typeface>> TYPEFACE_CACHE = new HashMap<>();
 
-	static final int MODE_PAGING = 1;
-	static final int MODE_SLIDING = 2;
-
-
-	private Adapter mAdapter;
-	private RecyclerView mImpl;
+	private Renderer mRenderer;
 
 	public TeView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
@@ -36,16 +41,6 @@ public class TeView extends FrameLayout {
 
 	public TeView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
-
-		mImpl = new RecyclerView(context);
-		mImpl.setClipToPadding(false);
-		mImpl.setClipChildren(false);
-		mImpl.setLayoutManager(new LinearLayoutManager(context));
-		addView(mImpl, new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-
-		mAdapter = new Adapter(context);
-		mImpl.setAdapter(mAdapter);
-
 		init(context, attrs, defStyleAttr);
 	}
 
@@ -53,96 +48,140 @@ public class TeView extends FrameLayout {
 		@SuppressLint("CustomViewStyleable")
 		TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.me_chan_te_TeView, defStyleAttr, 0);
 		try {
-			init(typedArray);
+			init(context, typedArray);
 		} finally {
 			typedArray.recycle();
 		}
 	}
 
-	private void init(TypedArray typedArray) {
-		float segmentSpace = typedArray.getDimension(R.styleable.me_chan_te_TeView_segmentSpace, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 22, getResources().getDisplayMetrics()));
-		mImpl.addItemDecoration(new SpaceItemDecoration(segmentSpace));
+	private void init(Context context, TypedArray typedArray) {
+		Resources resources = getResources();
+		RenderOption renderOption = new RenderOption();
 
-		int mode = typedArray.getInt(R.styleable.me_chan_te_TeView_segmentSpace, MODE_SLIDING);
-		if (mode == MODE_PAGING) {
-			PagerSnapHelper pagerSnapHelper = new PagerSnapHelper();
-			pagerSnapHelper.attachToRecyclerView(mImpl);
+		// 设置字体颜色
+		renderOption.setTextColor(
+				typedArray.getColor(R.styleable.me_chan_te_TeView_me_chan_te_textColor,
+						ContextCompat.getColor(context, R.color.me_chan_te_text_color)
+				)
+		);
+
+		// 设置字体
+		String typefacePath = typedArray.getString(R.styleable.me_chan_te_TeView_me_chan_te_typefaceAssets);
+		if (TextUtils.isEmpty(typefacePath)) {
+			typefacePath = "typeface/SourceSerifPro-Regular.ttf";
 		}
-
-		float textSize = typedArray.getDimension(R.styleable.me_chan_te_TeView_textSize, TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 18, getResources().getDisplayMetrics()));
-		mAdapter.setTextSize(textSize);
-
-		int breakStrategy = typedArray.getInt(R.styleable.me_chan_te_TeView_breakStrategy, BREAK_STRATEGY_BALANCE);
-		if (breakStrategy == BREAK_STRATEGY_SIMPLE) {
-			mAdapter.setBreakStrategy(BreakStrategy.SIMPLE);
+		WeakReference<Typeface> typefaceWeakReference = TYPEFACE_CACHE.get(typefacePath);
+		Typeface typeface;
+		if (typefaceWeakReference != null && (typeface = typefaceWeakReference.get()) != null) {
+			renderOption.setTypeface(typeface);
 		} else {
-			mAdapter.setBreakStrategy(BreakStrategy.BALANCED);
+			typeface = Typeface.createFromAsset(context.getAssets(), "typeface/SourceSerifPro-Regular.ttf");
+			typefaceWeakReference = new WeakReference<>(typeface);
+			TYPEFACE_CACHE.put(typefacePath, typefaceWeakReference);
+			renderOption.setTypeface(typeface);
 		}
 
-		boolean wordSelectable = typedArray.getBoolean(R.styleable.me_chan_te_TeView_wordSelectable, false);
-		// TODO
+		// 设置字体大小
+		renderOption.setTextSize(
+				typedArray.getDimension(R.styleable.me_chan_te_TeView_me_chan_te_textSize,
+						TypedValue.applyDimension(
+								TypedValue.COMPLEX_UNIT_SP,
+								DEFAULT_TEXT_SIZE,
+								resources.getDisplayMetrics()
+						)
+				)
+		);
 
+		// 行间距
+		renderOption.setLineSpace(
+				typedArray.getDimension(R.styleable.me_chan_te_TeView_me_chan_te_lineSpace,
+						TypedValue.applyDimension(
+								TypedValue.COMPLEX_UNIT_DIP,
+								DEFAULT_LINE_SPACE,
+								resources.getDisplayMetrics()
+						)
+				)
+		);
 
+		// 是否开启首行缩进
+		renderOption.setIndentEnable(
+				typedArray.getBoolean(R.styleable.me_chan_te_TeView_me_chan_te_indent, false)
+		);
+
+		// 选中字体的背景色
+		renderOption.setSelectedBackgroundColor(
+				typedArray.getColor(R.styleable.me_chan_te_TeView_me_chan_te_selectedBackgroundColor,
+						ContextCompat.getColor(context, R.color.me_chan_te_theme_color)
+				)
+		);
+
+		// 选中字体的颜色
+		renderOption.setSelectedTextColor(
+				typedArray.getColor(R.styleable.me_chan_te_TeView_me_chan_te_selectedTextColor, Color.WHITE)
+		);
+
+		// 片段间的间隔
+		renderOption.setSegmentSpace(typedArray.getDimension(R.styleable.me_chan_te_TeView_me_chan_te_segmentSpace,
+				TypedValue.applyDimension(
+						TypedValue.COMPLEX_UNIT_DIP,
+						DEFAULT_SEGMENT_SPACE,
+						resources.getDisplayMetrics())
+				)
+		);
+
+		// 断字策略
+		int breakStrategy = typedArray.getInt(R.styleable.me_chan_te_TeView_me_chan_te_breakStrategy, BREAK_STRATEGY_SIMPLE);
+		renderOption.setBreakStrategy(
+				breakStrategy == BREAK_STRATEGY_BALANCE ?
+						BreakStrategy.BALANCED : BreakStrategy.SIMPLE
+		);
+
+		// 是否可选单词
+		renderOption.setWordSelectable(
+				typedArray.getBoolean(R.styleable.me_chan_te_TeView_me_chan_te_wordSelectable, true)
+		);
+
+		int mode = typedArray.getInt(R.styleable.me_chan_te_TeView_me_chan_te_segmentSpace, MODE_SLIDING);
+		if (mode == MODE_PAGING) {
+			mRenderer = new PagingRenderer(this, renderOption);
+		} else {
+			mRenderer = new SlidingRenderer(this, renderOption);
+		}
 	}
 
 	public void setSource(final Source source) {
 		int width = getWidth();
-		if (width <= 0) {
+		int height = getHeight();
+		if (width <= 0 || height <= 0) {
 			getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 				@Override
 				public void onGlobalLayout() {
 					getViewTreeObserver().removeOnGlobalLayoutListener(this);
-					mAdapter.render(source, getWidth());
+					mRenderer.render(source, getWidth(), getHeight());
 				}
 			});
 			return;
 		}
-		mAdapter.render(source, width);
+
+		mRenderer.render(source, width, height);
 	}
 
-	/**
-	 * @param textSize sp unit
-	 */
-	public void setTextSize(float textSize) {
-		setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+	public RenderOption createRendererOption() {
+		return mRenderer.createRendererOption();
 	}
 
-	/**
-	 * @param unit     {@link TypedValue#COMPLEX_UNIT_PX} etc
-	 * @param textSize text size
-	 */
-	public void setTextSize(int unit, float textSize) {
-		float px = TypedValue.applyDimension(unit, textSize, getResources().getDisplayMetrics());
-		mAdapter.setTextSize(px);
+	public void setParser(Parser<?> parser) {
+		mRenderer.setParser(parser);
 	}
 
-	public void setParser(Parser parser) {
-		mAdapter.setParser(parser);
-	}
-
-	public void setDebugMode(boolean debugMode) {
-		mAdapter.setDebugMode(debugMode);
-	}
-
-	public boolean isDebugMode() {
-		return mAdapter.isDebugMode();
-	}
-
-	public void setBreakStrategy(BreakStrategy breakStrategy) {
-		mAdapter.setBreakStrategy(breakStrategy);
-	}
-
-	public void setTypeface(Typeface typeface) {
-		mAdapter.setTypeface(typeface);
-	}
-
-	public void setTextColor(int color) {
-		mAdapter.setTextColor(color);
+	public void refresh(RenderOption renderOption) {
+		mRenderer.refresh(renderOption);
 	}
 
 	@Override
 	protected void onDetachedFromWindow() {
-		mAdapter.release();
+		// TODO need test
+		mRenderer.release();
 		super.onDetachedFromWindow();
 	}
 }
