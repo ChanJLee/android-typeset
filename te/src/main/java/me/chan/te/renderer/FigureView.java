2ba01167387303cbd4090e72a4b83392ebcc7557
chan <1355425625@qq.com>
2019-11-09 23:43:52 +0800

    add figure view

---
+++ te/src/main/java/me/chan/te/renderer/FigureView.java
@@ -0,0 +1,40 @@
+package me.chan.te.renderer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import me.chan.te.image.ImageLoader;
+import me.chan.te.text.Figure;
+
+public class FigureView extends android.support.v7.widget.AppCompatImageView {
+	private Figure mFigure;
+
+	public FigureView(Context context) {
+		super(context);
+	}
+
+	public FigureView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public FigureView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public void render(ImageLoader imageLoader, Figure figure) {
+		mFigure = figure;
+		imageLoader.uri(figure.getUrl())
+				.size((int) figure.getWidth(), (int) figure.getHeight())
+				.into(this);
+		requestLayout();
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		if (mFigure != null) {
+			widthMeasureSpec = MeasureSpec.makeMeasureSpec((int) mFigure.getWidth(), MeasureSpec.EXACTLY);
+			heightMeasureSpec = MeasureSpec.makeMeasureSpec((int) mFigure.getHeight(), MeasureSpec.EXACTLY);
+		}
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+}
