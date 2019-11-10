b3d26fd6d87614230ca195eb940ce181185e5ef9
chan <1355425625@qq.com>
2019-11-10 19:35:08 +0800

    refactor book parser

--- te/src/main/java/me/chan/te/parser/TextParser.java
+++ te/src/main/java/me/chan/te/parser/TextParser.java
@@ -3,14 +3,10 @@ package me.chan.te.parser;
 import android.support.annotation.NonNull;
 
 import me.chan.te.config.Option;
-import me.chan.te.parser.utils.PlainTextParserUtils;
-import me.chan.te.text.Document;
-import me.chan.te.text.Paragraph;
 import me.chan.te.hypher.Hypher;
 import me.chan.te.measurer.Measurer;
-
-import static me.chan.te.parser.utils.PlainTextParserUtils.findNewline;
-import static me.chan.te.parser.utils.PlainTextParserUtils.skipBlank;
+import me.chan.te.text.Document;
+import me.chan.te.text.Paragraph;
 
 public class TextParser implements Parser<CharSequence> {
 	@Override
@@ -22,11 +18,54 @@ public class TextParser implements Parser<CharSequence> {
 			int last = findNewline(charSequence, i, len);
 			if (i != last) {
 				Paragraph.Builder builder = Paragraph.Builder.newBuilder(measurer, hypher, option, null);
-				PlainTextParserUtils.parse(charSequence, i, last, builder);
+				parse(charSequence, i, last, builder);
 				document.addSegment(builder.build());
 			}
 			i = skipBlank(charSequence, last, len);
 		}
 		return document;
+	}
+
+	private static void parse(CharSequence paragraph, int start, int end, Paragraph.Builder builder) {
+		for (int i = start; i < end; ) {
+			int last = findWord(paragraph, i, end);
+			int first = i;
+			i = skipBlank(paragraph, last, end);
+			if (first == last) {
+				continue;
+			}
+
+			builder.text(paragraph, first, last, null, null, null, null);
+		}
+	}
+
+	private static int findWord(CharSequence charSequence, int start, int end) {
+		return skip(charSequence, start, end, true);
+	}
+
+	private static int skipBlank(CharSequence charSequence, int start, int end) {
+		return skip(charSequence, start, end, false);
+	}
+
+	private static int findNewline(CharSequence charSequence, int start, int end) {
+		for (; start < end; ++start) {
+			char ch = charSequence.charAt(start);
+			if (ch == '\n') {
+				break;
+			}
+		}
+		return start;
+	}
+
+	private static int skip(CharSequence charSequence, int start, int end, boolean untilBlank) {
+		for (; start < end; ++start) {
+			char ch = charSequence.charAt(start);
+			boolean isBlank = ch == ' ' || ch == '\t' ||
+					ch == '\r' || ch == '\n';
+			if (isBlank == untilBlank) {
+				break;
+			}
+		}
+		return start;
 	}
 }
