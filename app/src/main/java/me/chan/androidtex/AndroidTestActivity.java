ca7784d53edde0e8cc6e54bdc806914b96ec82af
chan <1355425625@qq.com>
2019-11-12 21:43:12 +0800

    add android layout test code

--- app/src/main/java/me/chan/androidtex/AndroidTestActivity.java
+++ app/src/main/java/me/chan/androidtex/AndroidTestActivity.java
@@ -1,7 +1,13 @@
 package me.chan.androidtex;
 
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import me.chan.te.log.Log;
 
 public class AndroidTestActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class AndroidTestActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_android_test);
+
+		final TextView textView = findViewById(R.id.text);
+		textView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Rect rect = new Rect();
+				int count = textView.getLineCount();
+				for (int i = 0; i < count; ++i) {
+					textView.getLineBounds(i, rect);
+					Log.d("chan_debug", "index " + i + "height: " + rect.height());
+				}
+
+				Paint.FontMetrics fontMetrics = textView.getPaint().getFontMetrics();
+				Log.d("chan_debug", (fontMetrics.descent - fontMetrics.ascent) +
+						" descent" + fontMetrics.descent + " ascent" + fontMetrics.ascent +
+						"top " + fontMetrics.top + " bottom" + fontMetrics.bottom);
+			}
+		});
 	}
 }
