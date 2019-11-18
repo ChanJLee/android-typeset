ad8cb1238bb837aff495b8b5df5fcd2ba461adea
ChanJLee <1355425625@qq.com>
2019-11-18 21:54:42 +0800

    update active list unit test

--- te/src/test/java/me/chan/te/test/ActiveNodeListUnitTest.java
+++ te/src/test/java/me/chan/te/test/ActiveNodeListUnitTest.java
@@ -1,4 +1,4 @@
-package me.chan.te;
+package me.chan.te.test;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -37,6 +37,7 @@ public class ActiveNodeListUnitTest {
 		Assert.assertSame(activeNodeList.getHeader(), activeNodeList.getTail());
 
 		checkAddRemoveSide(activeNodeList);
+		checkAddRemoveRandom(activeNodeList);
 	}
 
 	private void checkAddRemoveSide(ActiveNodeList activeNodeList) {
@@ -72,6 +73,42 @@ public class ActiveNodeListUnitTest {
 		activeNodeList.remove(activeNodeList.getHeader());
 		checkContent(activeNodeList, 2);
 		activeNodeList.remove(activeNodeList.getTail());
+		Assert.assertTrue(activeNodeList.isEmpty());
+		Assert.assertNull(activeNodeList.getHeader());
+		Assert.assertNull(activeNodeList.getTail());
+	}
+
+	private void checkAddRemoveRandom(ActiveNodeList activeNodeList) {
+		Node node1 = createNode(1);
+		Node node2 = createNode(2);
+		Node node3 = createNode(3);
+		Node node4 = createNode(4);
+
+		activeNodeList.pushBack(node2);
+		activeNodeList.insertBefore(node2, node1);
+		activeNodeList.pushBack(node4);
+		activeNodeList.insertBefore(node4, node3);
+
+		checkContent(activeNodeList, 1, 2, 3, 4);
+		Assert.assertSame(node1, activeNodeList.getHeader());
+		Assert.assertSame(node4, activeNodeList.getTail());
+
+		activeNodeList.remove(node2);
+		checkContent(activeNodeList, 1, 3, 4);
+		Assert.assertSame(node1, activeNodeList.getHeader());
+		Assert.assertSame(node4, activeNodeList.getTail());
+
+		activeNodeList.remove(node4);
+		checkContent(activeNodeList, 1, 3);
+		Assert.assertSame(node1, activeNodeList.getHeader());
+		Assert.assertSame(node3, activeNodeList.getTail());
+
+		activeNodeList.remove(node1);
+		checkContent(activeNodeList, 3);
+		Assert.assertSame(node3, activeNodeList.getHeader());
+		Assert.assertSame(node3, activeNodeList.getTail());
+
+		activeNodeList.remove(node3);
 		Assert.assertTrue(activeNodeList.isEmpty());
 		Assert.assertNull(activeNodeList.getHeader());
 		Assert.assertNull(activeNodeList.getTail());
