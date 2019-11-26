b641e4d19be675af11074b6cdaf98cef505bde5c
jiacheng.li <jiacheng.li@shanbay.com>
2019-11-26 10:36:48 +0800

    add sent select

--- te/src/main/java/me/chan/te/parser/BookParser.java
+++ te/src/main/java/me/chan/te/parser/BookParser.java
@@ -17,9 +17,11 @@ import java.util.regex.Pattern;
 
 import me.chan.te.R;
 import me.chan.te.hypher.Hypher;
+import me.chan.te.log.Log;
 import me.chan.te.measurer.Measurer;
 import me.chan.te.text.Document;
 import me.chan.te.text.Figure;
+import me.chan.te.text.OnClickedListener;
 import me.chan.te.text.TextAttribute;
 import me.chan.te.text.Paragraph;
 import me.chan.te.text.UnderLine;
@@ -202,26 +204,39 @@ public class BookParser implements Parser<CharSequence> {
 
 	private void parseSent(XmlPullParser parser, Paragraph.Builder builder) throws IOException, XmlPullParserException {
 		parser.require(XmlPullParser.START_TAG, null, "sent");
-		String id = parser.getAttributeValue(null, "id");
+		final String id = parser.getAttributeValue(null, "id");
+		OnClickedListener sentOnClickedListener = new OnClickedListener() {
+			@Override
+			public boolean onClicked(float x, float y) {
+				Log.d("BookParser", "select sent: " + id);
+				return true;
+			}
+		};
 		String text = safeNextText(parser);
 		if (!TextUtils.isEmpty(text)) {
-			parseParagraph(builder, text, id);
+			parseParagraph(builder, text, sentOnClickedListener);
 		}
 		parser.require(XmlPullParser.END_TAG, null, "sent");
 	}
 
-	private void parseParagraph(Paragraph.Builder builder, String paragraph, String id) {
+	private void parseParagraph(Paragraph.Builder builder, String paragraph, OnClickedListener spanListener) {
 		String[] strings = PATTERN.split(paragraph);
 		for (int i = 0; strings != null && i < strings.length; ++i) {
-			String text = strings[i];
+			final String text = strings[i];
 			if (TextUtils.isEmpty(text)) {
 				continue;
 			}
 
-			builder.newSpanBuilder(null)
+			builder.newSpanBuilder(spanListener)
 					.next(text)
 					.setForeground(UnderLine.obtain(Color.RED))
-					.setExtra(id)
+					.setOnClickedListener(new OnClickedListener() {
+						@Override
+						public boolean onClicked(float x, float y) {
+							Log.d("BookParser", "click: " + text);
+							return true;
+						}
+					})
 					.buildSpan();
 		}
 	}
