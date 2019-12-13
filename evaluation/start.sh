bcc2e0af429bd53f5409ca8fe40dbe7b90650bd9
jiacheng.li <jiacheng.li@shanbay.com>
2019-12-13 17:28:56 +0800

    update tex analyze

--- evaluation/start.sh
+++ evaluation/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+adb logcat -c
+cout=0
 adb logcat | while read line;
 do 
     if [[ ${line} != *EvaluationSample* ]]; then
         continue;
     fi
     echo ${line};
+    ((i++));
     path=`echo ${line} | awk -F ' ' '{print $10}'`;
     python2 ./evaluation.py "figure-${i}" ${path} &
 done
