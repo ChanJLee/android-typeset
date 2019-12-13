2335dd3a02c342314d93a645715a0577305de10c
jiacheng.li <jiacheng.li@shanbay.com>
2019-12-13 16:13:35 +0800

    add python evaluation

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -5,6 +5,8 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
+import subprocess
+import os
 
 def render(samples): 
     n, bins, patches = plt.hist(samples, 10, facecolor='blue', alpha=0.5)
@@ -16,6 +18,16 @@ def render(samples):
     plt.grid(True)
     plt.show()
 
+def load(path):
+    try:
+        s = os.popen("adb shell cat " + path).read()
+        render(json.loads(s))
+    except Exception as e:
+        print e.message
 
 if __name__ == '__main__':
-    render(json.loads("[0, 1, 2, 2, 2.5, 3]"))
+    proc = subprocess.Popen(['adb','logcat'], stdout=subprocess.PIPE)
+    for line in proc.stdout:
+        if "EvaluationSample" in line:
+            print line.split()
+            load(line.split("EvaluationSample")[1].strip())
