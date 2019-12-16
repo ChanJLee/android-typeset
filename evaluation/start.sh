f5aa121f0e6fa250c947c45fdefa89b70e2cb312
jiacheng.li <jiacheng.li@shanbay.com>
2019-12-16 10:03:21 +0800

    update penalty value

--- evaluation/start.sh
+++ evaluation/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 adb logcat -c
-cout=0
+count=0
 adb logcat | while read line;
 do 
     if [[ ${line} != *EvaluationSample* ]]; then
         continue;
     fi
     echo ${line};
-    ((i++));
+    ((count++));
     path=`echo ${line} | awk -F ' ' '{print $10}'`;
     echo "read ${path}"
-    python2 ./evaluation.py "figure-${i}" ${path} &
+    python2 ./evaluation.py "figure-${count}" ${path} &
 done
