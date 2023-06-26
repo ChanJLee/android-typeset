4ba4a5031a3c642dc7b5a751dcddb2d63d85cdb7
jiacheng.li <jiacheng.li@shanbay.com>
2023-06-26 17:08:58 +0800

    add stage

--- app/src/main/java/me/chan/androidtex/MainActivity.java
+++ app/src/main/java/me/chan/androidtex/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 		findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(v.getContext(), TexasViewDemoActivity.class);
+				Intent intent = new Intent(v.getContext(), ParagraphRenderActivity.class);
 				startActivity(intent);
 			}
 		});
