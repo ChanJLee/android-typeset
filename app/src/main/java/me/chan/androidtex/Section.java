e4e64ae0b4d529dbdab7b614bc47dbb9cf04f568
ChanJLee <wuliyichen@gmail.com>
2023-06-28 20:57:06 +0800

    support render section

--- app/src/main/java/me/chan/androidtex/Section.java
+++ app/src/main/java/me/chan/androidtex/Section.java
@@ -1,4 +1,16 @@
 package me.chan.androidtex;
 
+import androidx.annotation.Keep;
+
+import java.util.List;
+
+@Keep
 public class Section {
+    public List<Chapter> content;
+    @Keep
+    public static class Chapter {
+        public String type;
+        public String title;
+        public String content;
+    }
 }
--- app/src/main/java/me/chan/androidtex/api/NiceBookApi.java
+++ app/src/main/java/me/chan/androidtex/api/NiceBookApi.java
@@ -3,10 +3,15 @@ package me.chan.androidtex.api;
 import java.util.Map;
 
 import io.reactivex.Observable;
+import me.chan.androidtex.Section;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface NiceBookApi {
 
     @GET("/material/meta.json")
     Observable<Map<String, Object>> fetchBooks();
+
+    @GET("/material/{bookId}/{sectionId}/content.json")
+    Observable<Section> fetchSection(@Path("bookId") String bookId, @Path("sectionId") String sectionId);
 }
