6fca5c23eddb48fe36552f915e785c7a3e3d5994
ChanJLee <wuliyichen@gmail.com>
2023-10-30 22:08:08 +0800

    fix source compile error

--- library/src/main/java/me/chan/texas/source/Source.java
+++ library/src/main/java/me/chan/texas/source/Source.java
@@ -1,6 +1,7 @@
 package me.chan.texas.source;
 
 import androidx.annotation.AnyThread;
+import androidx.annotation.Nullable;
 
 import me.chan.texas.renderer.LoadingStrategy;
 
@@ -9,15 +10,27 @@ import me.chan.texas.renderer.LoadingStrategy;
  *
  * @param <T> 返回的类型
  */
-public interface Source<T> {
+public abstract class Source<T> {
+
+	private volatile boolean mClosed = false;
+
 	/**
 	 * 打开开始读取内容
 	 *
-	 * @return 数据
-	 * @throws SourceOpenException 异常的时候抛出
+	 * @return 数据 返回空则代表已经没有根多数据
+	 * @throws Throwable 异常的时候抛出
 	 */
 	@AnyThread
-	T open(LoadingStrategy strategy) throws SourceOpenException;
+	@Nullable
+	public synchronized T open(LoadingStrategy strategy) throws SourceOpenException {
+		if (mClosed) {
+			throw new SourceOpenException(-1, "source has been closed", null);
+		}
+
+		return onOpen(strategy);
+	}
+
+	protected abstract T onOpen(LoadingStrategy strategy) throws SourceOpenException;
 
 	/**
 	 * 读取完毕，关闭source
@@ -25,5 +38,19 @@ public interface Source<T> {
 	 * @throws SourceCloseException
 	 */
 	@AnyThread
-	void close() throws SourceCloseException;
+	public synchronized final void close() {
+		if (mClosed) {
+			return;
+		}
+
+		try {
+			onClose();
+		} catch (Throwable ignore) {
+			/* noop */
+		} finally {
+			mClosed = true;
+		}
+	}
+
+	protected abstract void onClose() throws SourceCloseException;
 }
