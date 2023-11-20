e97d328d0da94e3979bca9fbb2461efedf013dd5
ChanJLee <wuliyichen@gmail.com>
2023-11-20 22:30:27 +0800

    save process

--- library/src/main/java/me/chan/texas/renderer/ui/indicator/LoadingIndicator.java
+++ library/src/main/java/me/chan/texas/renderer/ui/indicator/LoadingIndicator.java
@@ -2,11 +2,7 @@ package me.chan.texas.renderer.ui.indicator;
 
 public interface LoadingIndicator {
 
-    void renderStart();
-
-    void renderError();
-
-    void renderSuccess();
+    void renderLoading();
 
     void dismiss();
 }
