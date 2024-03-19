25f9ad0a0a8d3e7fa36ad3a237083c508278b61d
jiacheng.li <jiacheng.li@shanbay.com>
2024-03-19 18:30:35 +0800

    fix unit test

--- library/src/test/java/com/shanbay/lib/texas/test/SourceUnitTest.java
+++ library/src/test/java/com/shanbay/lib/texas/test/SourceUnitTest.java
@@ -1,9 +1,7 @@
 package com.shanbay.lib.texas.test;
 
-import me.chan.texas.source.ObjectSource;
-import me.chan.texas.source.SourceCloseException;
-import me.chan.texas.source.SourceOpenException;
-import me.chan.texas.source.StreamTextSource;
+import static org.junit.Assert.assertFalse;
+
 import com.shanbay.lib.texas.test.mock.MockFileInputStream;
 
 import org.junit.Assert;
@@ -12,84 +10,72 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
-import static org.junit.Assert.assertFalse;
+import me.chan.texas.renderer.LoadingStrategy;
+import me.chan.texas.source.SourceOpenException;
+import me.chan.texas.source.StreamTextSource;
 
 public class SourceUnitTest {
 
-//	@Test
-//	public void test() throws IOException {
-//		File file = new File("../app/src/main/assets/TheBookAndTheSword.txt");
-//		FileInputStream fileInputStream = new FileInputStream(file);
-//		StreamTextSource streamSource = new StreamTextSource(fileInputStream);
-//		CharSequence charSequence = null;
-//		try {
-//			charSequence = streamSource.open();
-//		} catch (SourceOpenException e) {
-//		}
-//
-//		Assert.assertNotNull(charSequence);
-//
-//		try {
-//			streamSource.close();
-//		} catch (SourceCloseException e) {
-//		}
-//
-//		try {
-//			fileInputStream.read();
-//			Assert.fail("test stream close failed");
-//		} catch (Throwable throwable) {
-//			assertFalse(throwable instanceof AssertionError);
-//		}
-//
-//		fileInputStream = new MockFileInputStream(file);
-//		try {
-//			streamSource = new StreamTextSource(fileInputStream);
-//			streamSource.open();
-//			Assert.fail("test read bad file failed");
-//		} catch (SourceOpenException e) {
-//		}
-//	}
-//
-//	@Test
-//	public void testCacheSource() throws SourceOpenException, SourceCloseException {
-//		String str = "hello";
-//		CountSource countSource = new CountSource(str);
-//		CacheSource<String> cacheSource = new CacheSource<>(countSource);
-//		Assert.assertEquals(str, cacheSource.open());
-//		Assert.assertEquals(countSource.openCount, 1);
-//		Assert.assertEquals(countSource.closeCount, 0);
-//		cacheSource.close();
-//		Assert.assertEquals(countSource.closeCount, 1);
-//
-//		// 读第二次测试幂等性
-//		Assert.assertEquals(str, cacheSource.open());
-//		Assert.assertEquals(countSource.openCount, 1);
-//
-//		cacheSource.cleanCache();
-//		Assert.assertEquals(str, cacheSource.open());
-//		Assert.assertEquals(countSource.openCount, 2);
-//		Assert.assertEquals(countSource.closeCount, 1);
-//	}
-//
-//	private static class CountSource extends ObjectSource<String> {
-//		public int openCount = 0;
-//		public int closeCount = 0;
-//
-//		public CountSource(String object) {
-//			super(object);
-//		}
-//
-//		@Override
-//		public String open() throws SourceOpenException {
-//			openCount++;
-//			return super.open();
-//		}
-//
-//		@Override
-//		public void close() throws SourceCloseException {
-//			closeCount++;
-//			super.close();
-//		}
-//	}
+	@Test
+	public void test() throws IOException {
+		File file = new File("../app/src/main/assets/TheBookAndTheSword.txt");
+		FileInputStream fileInputStream = new FileInputStream(file);
+		StreamTextSource streamSource = new StreamTextSource(fileInputStream);
+		CharSequence charSequence = null;
+		try {
+			charSequence = streamSource.open(LoadingStrategy.LOAD);
+		} catch (SourceOpenException e) {
+		}
+
+		Assert.assertNotNull(charSequence);
+
+		streamSource.close();
+
+		try {
+			fileInputStream.read();
+			Assert.fail("test stream close failed");
+		} catch (Throwable throwable) {
+			assertFalse(throwable instanceof AssertionError);
+		}
+
+		fileInputStream = new MockFileInputStream(file);
+		try {
+			streamSource = new StreamTextSource(fileInputStream);
+			streamSource.open(LoadingStrategy.LOAD);
+			Assert.fail("test read bad file failed");
+		} catch (SourceOpenException e) {
+		}
+	}
+
+	@Test
+	public void testLoadingStrategy() {
+		StreamTextSource streamSource = new StreamTextSource(new MockInputStream(), 1);
+		CharSequence charSequence = null;
+		try {
+			charSequence = streamSource.open(LoadingStrategy.LOAD);
+			System.out.println(charSequence);
+		} catch (SourceOpenException e) {
+		}
+
+		Assert.assertNotNull(charSequence);
+
+		streamSource.close();
+	}
+
+	private static class MockInputStream extends InputStream {
+
+		private final byte[] mBytes = "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n".getBytes();
+		private int mPtr;
+
+		@Override
+		public int read() throws IOException {
+			return mPtr >= mBytes.length ? -1 : mBytes[mPtr++];
+		}
+
+		public void adjust(int offset) {
+			mPtr += offset;
+		}
+	}
 }
