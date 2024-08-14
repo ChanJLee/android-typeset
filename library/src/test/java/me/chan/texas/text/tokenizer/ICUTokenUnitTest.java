94893970a0175050b7aad78814ed0eabbbf036fe
ChanJLee <wuliyichen@gmail.com>
2024-08-14 22:26:22 +0800

    support bidi

--- library/src/test/java/me/chan/texas/text/tokenizer/ICUTokenUnitTest.java
+++ library/src/test/java/me/chan/texas/text/tokenizer/ICUTokenUnitTest.java
@@ -1,8 +1,10 @@
 package me.chan.texas.text.tokenizer;
 
-import org.junit.Test;
+import com.ibm.icu.text.Bidi;
+import com.ibm.icu.text.BidiRun;
+import com.ibm.icu.text.BreakIterator;
 
-import java.text.BreakIterator;
+import org.junit.Test;
 
 public class ICUTokenUnitTest {
 
@@ -10,6 +12,7 @@ public class ICUTokenUnitTest {
 	public void test() {
 		String msg = "\u0067\u0308 fuck你好😜，你好真的假的？تم تكرار كلمة 'كوب' بشكل غير ضروري.";
 		print(msg);
+		print2(msg);
 	}
 
 	private void print(String source) {
@@ -21,6 +24,15 @@ public class ICUTokenUnitTest {
 			 end != BreakIterator.DONE;
 			 start = end, end = boundary.next()) {
 			System.out.println("(" + start + "," + end + ")[" + source.substring(start, end) + "]");
+		}
+	}
+
+	private void print2(String source) {
+		Bidi bidi = new Bidi();
+		bidi.setPara(source, Bidi.LEVEL_DEFAULT_LTR, null);
+		for (int i = 0; i < bidi.countRuns(); ++i) {
+			BidiRun run = bidi.getVisualRun(i);
+			System.out.println(run + "-[" + source.substring(run.getStart(), run.getLimit()) + "]");
 		}
 	}
 }
