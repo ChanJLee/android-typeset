7c0a251d50b7a2daccf865544d5bf1801dfa37f3
ChanJLee <wuliyichen@gmail.com>
2024-08-18 19:44:31 +0800

    support word reader

--- library/src/test/java/me/chan/texas/text/tokenizer/ICUTokenUnitTest.java
+++ library/src/test/java/me/chan/texas/text/tokenizer/ICUTokenUnitTest.java
@@ -9,10 +9,12 @@ import org.junit.Test;
 public class ICUTokenUnitTest {
 
 	@Test
-	public void test() {
-		String msg = "\u0067\u0308 fuck你好😜，你好真的假的？تم تكرار كلمة 'كوب' بشكل غير ضروري.";
+	public void testSimple() {
+//		String msg = "\u0067\u0308 fuck你好😜，你好真的假的？تم تكرار كلمة 'كوب' بشكل غير ضروري.";
+		String msg = "나는  Chicago에  산다.100“get on”这里指“应付，过活”，“can't get on without”即为“失去……便寸步难行，不能没有……”。这里Laurie在极力表明Jo和他在一起是“全村的期望”，他不能没有她。don't.";
 		print(msg);
 		print2(msg);
+		print3(msg);
 	}
 
 	private void print(String source) {
@@ -33,6 +35,19 @@ public class ICUTokenUnitTest {
 		for (int i = 0; i < bidi.countRuns(); ++i) {
 			BidiRun run = bidi.getVisualRun(i);
 			System.out.println(run + "-[" + source.substring(run.getStart(), run.getLimit()) + "]");
+		}
+	}
+
+	private void print3(String source) {
+		System.out.println("========== word instance");
+		BreakIterator boundary = BreakIterator.getWordInstance();
+		boundary.setText(source);
+
+		int start = boundary.first();
+		for (int end = boundary.next();
+			 end != BreakIterator.DONE;
+			 start = end, end = boundary.next()) {
+			System.out.println("(" + start + "," + end + ")[" + source.substring(start, end) + "] + " + boundary.getRuleStatus());
 		}
 	}
 }
