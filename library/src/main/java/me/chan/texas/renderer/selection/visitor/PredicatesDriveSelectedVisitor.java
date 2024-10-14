e905604b3f94a27009a17be5049ff8e1a8059d99
jiacheng.li <jiacheng.li@shanbay.com>
2024-10-14 13:37:38 +0800

    support selection user-define styles

--- library/src/main/java/me/chan/texas/renderer/selection/visitor/PredicatesDriveSelectedVisitor.java
+++ library/src/main/java/me/chan/texas/renderer/selection/visitor/PredicatesDriveSelectedVisitor.java
@@ -2,25 +2,40 @@ package me.chan.texas.renderer.selection.visitor;
 
 import android.graphics.RectF;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RestrictTo;
 
 import me.chan.texas.renderer.ParagraphPredicates;
 import me.chan.texas.renderer.RenderOption;
+import me.chan.texas.renderer.selection.ParagraphSelection;
 import me.chan.texas.text.Paragraph;
+import me.chan.texas.text.TextStyles;
 import me.chan.texas.text.layout.Box;
 
 @RestrictTo(RestrictTo.Scope.LIBRARY)
-public class SelfDriveSelectedVisitor extends SelectedVisitor {
+public class PredicatesDriveSelectedVisitor extends SelectedVisitor {
 	private ParagraphPredicates mPredicates;
 
-	public void reset(RenderOption renderOption, ParagraphPredicates predicates) {
+	public void reset(RenderOption renderOption, ParagraphPredicates predicates, @Nullable TextStyles styles) {
 		mPredicates = predicates;
-		super.reset(true, renderOption);
+
+		// 如果样式为空，则按照长按的效果来处理
+		if (styles == null) {
+			super.reset(true, renderOption);
+			return;
+		}
+
+		super.reset(styles, renderOption);
 	}
 
 	@Override
 	protected void onVisitParagraphStart(Paragraph paragraph) {
 		if (!mPredicates.acceptParagraph(paragraph.getTag())) {
+			ParagraphSelection prev = paragraph.getSelection();
+			if (prev != null) {
+				prev.recycle();
+			}
+			paragraph.setSelection(null);
 			sendVisitSig(SIG_STOP_PARA_VISIT);
 		}
 	}
