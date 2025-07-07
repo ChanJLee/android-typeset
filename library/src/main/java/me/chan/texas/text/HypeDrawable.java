7ee0e7551205889e908f3133a982659c04eef173
chan <chanlee@MacBook-Air-6.local>
2025-07-07 14:02:46 +0800

    update drawable box

---
+++ library/src/main/java/me/chan/texas/text/HypeDrawable.java
@@ -0,0 +1,68 @@
+package me.chan.texas.text;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import me.chan.texas.text.layout.StateList;
+
+public abstract class HypeDrawable {
+	private Object mTag;
+	private Appearance background;
+	private Appearance foreground;
+	private final float mWidth;
+	private final float mHeight;
+
+	public HypeDrawable(float width, float height) {
+		mWidth = width;
+		mHeight = height;
+	}
+
+	/**
+	 * @return 颜文字的宽
+	 */
+	public final float getWidth() {
+		return mWidth;
+	}
+
+	/**
+	 * @return 颜文字的高
+	 */
+	public final float getHeight() {
+		return mHeight;
+	}
+
+	public final void draw(Canvas canvas, Paint paint, float x, float y, StateList states) {
+		onDraw(canvas, paint, x, y, states, mTag);
+	}
+
+	protected abstract void onDraw(Canvas canvas, Paint paint, float x, float y, StateList states, Object tag);
+
+	/**
+	 * 设置唯一标识
+	 *
+	 * @param tag tag
+	 */
+	public final void setTag(Object tag) {
+		mTag = tag;
+	}
+
+	public final Object getTag() {
+		return mTag;
+	}
+
+	public final Appearance getBackground() {
+		return background;
+	}
+
+	public final void setBackground(Appearance background) {
+		this.background = background;
+	}
+
+	public final Appearance getForeground() {
+		return foreground;
+	}
+
+	public final void setForeground(Appearance foreground) {
+		this.foreground = foreground;
+	}
+}
--- library/src/main/java/me/chan/texas/text/layout/DrawableBox.java
+++ library/src/main/java/me/chan/texas/text/layout/DrawableBox.java
@@ -4,8 +4,6 @@ import static androidx.annotation.RestrictTo.Scope.LIBRARY;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.StateListDrawable;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RestrictTo;
@@ -13,8 +11,7 @@ import androidx.annotation.RestrictTo;
 import me.chan.texas.Texas;
 import me.chan.texas.measurer.Measurer;
 import me.chan.texas.misc.ObjectPool;
-import me.chan.texas.text.Appearance;
-import me.chan.texas.text.Emoticon;
+import me.chan.texas.text.HypeDrawable;
 import me.chan.texas.text.TextAttribute;
 
 /**
@@ -23,73 +20,38 @@ import me.chan.texas.text.TextAttribute;
 @RestrictTo(LIBRARY)
 public class DrawableBox extends Box {
 	private static final ObjectPool<DrawableBox> POOL = new ObjectPool<>(Texas.getMemoryOption().getEmoticonBufferSize());
-	private static final int[] STATE_PRESSED = {
-			android.R.attr.state_pressed,
-	};
-	private static final int[] STATE_NORMAL = {
-			-android.R.attr.state_pressed
-	};
 
-	private Drawable mDrawable;
-	private Emoticon mEmoticon;
+	private HypeDrawable mDrawable;
 
-	public DrawableBox(@NonNull Drawable drawable, float width, float height, Emoticon emoticon) {
-		super(width, height);
+	public DrawableBox(@NonNull HypeDrawable drawable) {
+		super(drawable.getWidth(), drawable.getHeight());
 		mDrawable = drawable;
-		mEmoticon = emoticon;
 	}
 
 	@Override
 	public void draw(Canvas canvas, Paint paint, float x, float y, StateList states) {
-		Drawable drawable = mDrawable;
-		if (mDrawable instanceof StateListDrawable) {
-			StateListDrawable stateListDrawable = (StateListDrawable) mDrawable;
-			stateListDrawable.setState(states.isSelected() ? STATE_PRESSED : STATE_NORMAL);
-			drawable = stateListDrawable.getCurrent();
-		}
-
-		drawable.setBounds((int) x, (int) (y - getHeight()), (int) (x + getWidth()), (int) y);
-		drawable.draw(canvas);
-	}
-
-	public Drawable getDrawable() {
-		return mDrawable;
-	}
-
-	public void setDrawable(Drawable drawable) {
-		mDrawable = drawable;
-	}
-
-	public Emoticon getEmoticon() {
-		return mEmoticon;
+		mDrawable.draw(canvas, paint, x, y, states);
 	}
 
 	@Override
 	protected void onRecycle() {
 		super.onRecycle();
 		mDrawable = null;
-		mEmoticon.recycle();
-		mEmoticon = null;
 		POOL.release(this);
 	}
 
-	public static DrawableBox obtain(Drawable drawable,
-									 float width, float height,
-									 Emoticon emoticon, Object tag,
-									 Appearance background,
-									 Appearance foreground) {
+	public static DrawableBox obtain(HypeDrawable drawable) {
 		DrawableBox drawableBox = POOL.acquire();
 		if (drawableBox == null) {
-			drawableBox = new DrawableBox(drawable, width, height, emoticon);
+			drawableBox = new DrawableBox(drawable);
 		}
 
-		drawableBox.mWidth = width;
-		drawableBox.mHeight = height;
-		drawableBox.mTag = tag;
-		drawableBox.mBackground = background;
-		drawableBox.mForeground = foreground;
+		drawableBox.mWidth = drawable.getWidth();
+		drawableBox.mHeight = drawable.getHeight();
+		drawableBox.mTag = drawable.getTag();
+		drawableBox.mBackground = drawable.getBackground();
+		drawableBox.mForeground = drawable.getForeground();
 		drawableBox.mDrawable = drawable;
-		drawableBox.mEmoticon = emoticon;
 		drawableBox.reuse();
 		return drawableBox;
 	}
