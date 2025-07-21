a5c829f7aa2977ce2fc6b99696ab240c5dcd82ea
ChanJLee <wuliyichen@gmail.com>
2025-07-21 10:24:24 +0800

    add paint unit test

---
+++ library/src/test/java/me/chan/texas/renderer/core/graphics/CanvasUnitTest.java
@@ -0,0 +1,11 @@
+package me.chan.texas.renderer.core.graphics;
+
+import org.junit.Test;
+
+public class CanvasUnitTest {
+
+	@Test
+	public void test() {
+
+	}
+}
--- library/src/test/java/me/chan/texas/renderer/core/graphics/PaintUnitTest.java
+++ library/src/test/java/me/chan/texas/renderer/core/graphics/PaintUnitTest.java
@@ -1,6 +1,7 @@
 package me.chan.texas.renderer.core.graphics;
 
 import android.graphics.Color;
+import android.graphics.Paint;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -29,11 +30,249 @@ public class PaintUnitTest {
 		Assert.assertTrue(texasPaint.isModified());
 	}
 
+	/**
+	 * 参考 {@link TexasPaint} 接口，按照顺序编写测试用例，根据 {@link TexasPaintImpl#getPaint(boolean)} 的实现，判断是否修改了 paint
+	 */
 	@Test
 	public void testApi() {
 		MockTextPaint textPaint = new MockTextPaint();
 		textPaint.setMockTextSize(1);
 		PaintSet paintSet = new PaintSet(textPaint);
 		TexasPaintImpl texasPaint = new TexasPaintImpl();
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.reset();
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getFlags();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getHinting();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setHinting(Paint.HINTING_ON);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.isAntiAlias();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setAntiAlias(true);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.isDither();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setDither(true);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.isLinearText();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setLinearText(true);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.isSubpixelText();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setSubpixelText(true);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.isUnderlineText();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getUnderlinePosition();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getUnderlineThickness();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setUnderlineText(true);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.isStrikeThruText();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getStrikeThruPosition();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getStrikeThruThickness();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setStrikeThruText(true);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.isFakeBoldText();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setFakeBoldText(true);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.isFilterBitmap();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setFilterBitmap(true);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getStyle();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setStyle(Paint.Style.FILL);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getColor();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getColorLong();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setColor(Color.RED);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setColor(0xff00ff00);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getAlpha();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setAlpha(123);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setARGB(123, 123, 123, 123);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getStrokeWidth();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setStrokeWidth(123);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getStrokeMiter();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setStrokeMiter(123);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getStrokeCap();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setStrokeCap(Paint.Cap.BUTT);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getStrokeJoin();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setStrokeJoin(Paint.Join.BEVEL);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getFillPath(null, null);
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getShader();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setShader(null);
+		Assert.assertTrue(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.getColorFilter();
+		Assert.assertFalse(texasPaint.isModified());
+
+		texasPaint.reset(paintSet);
+		Assert.assertFalse(texasPaint.isModified());
+		texasPaint.setColorFilter(null);
+		Assert.assertTrue(texasPaint.isModified());
 	}
 }
