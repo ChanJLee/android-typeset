881e59525912c9d6cc10dcb18a885159b5f46ad6
ChanJLee <wuliyichen@gmail.com>
2025-07-21 17:04:16 +0800

    update dsl ui

--- app/src/main/java/me/chan/texas/ParagraphSourceKt.kt
+++ app/src/main/java/me/chan/texas/ParagraphSourceKt.kt
@@ -4,18 +4,18 @@ import me.chan.texas.renderer.ui.text.ParagraphView
 import me.chan.texas.text.Paragraph
 import me.chan.texas.text.dsl.para
 
-class ParagraphSourceKt(private val _index: Int): ParagraphView.ParagraphSource() {
+class ParagraphSourceKt(private val _index: Int) : ParagraphView.ParagraphSource() {
 
     override fun onRead(option: TexasOption): Paragraph {
         return para(option) {
             span("title ${_index}\n") {
-                style { textPaint, _ ->
-                    textPaint.isFakeBoldText = true
+                style { _ ->
+                    isFakeBoldText = true
                 }
 
-                foreground { canvas, paint, inner, outer, context ->
+                foreground { paint, inner, outer, context ->
                     paint.color = 0x11ff0000
-                    canvas.drawRect(inner.left, inner.top, inner.right, inner.bottom, paint)
+                    drawRect(inner.left, inner.top, inner.right, inner.bottom, paint)
                 }
             }
 
