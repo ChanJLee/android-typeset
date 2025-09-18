1b9da20ce3b14458383b0bcd9d84e22ca70a218d
chan <chanlee@MacBook-Air-6.local>
2025-09-18 17:33:51 +0800

    support document iterator unit test

--- library/src/main/java/me/chan/texas/text/util/TexasIterator.java
+++ library/src/main/java/me/chan/texas/text/util/TexasIterator.java
@@ -3,19 +3,30 @@ package me.chan.texas.text.util;
 import androidx.annotation.Nullable;
 
 public interface TexasIterator<T> {
+	/**
+	 * @return 返回下一个元素，如果没有则返回null
+	 */
 	@Nullable
 	T next();
 
+	/**
+	 * @return 返回前一个元素，如果没有则返回null
+	 */
 	@Nullable
 	T prev();
 
+	/**
+	 * @return 返回当前元素，如果没有则返回null
+	 */
 	@Nullable
 	T current();
 
+	/**
+	 * @param state {@link #save()}
+	 * @return 跳转到对应的state，并返回对应元素，如果跳转失败则不改变任何状态并返回null
+	 */
 	@Nullable
-	T seek(int state);
+	T restore(int state);
 
 	int save();
-
-	void restore(int state);
 }
