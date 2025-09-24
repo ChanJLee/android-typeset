495ab4de61437bfa4244f4d5b625654d20450e89
chan <chanlee@MacBook-Air-6.local>
2025-09-24 14:32:01 +0800

    refactor text bound compute

--- library/src/main/java/me/chan/texas/typesetter/ParagraphTypesetter.java
+++ library/src/main/java/me/chan/texas/typesetter/ParagraphTypesetter.java
@@ -49,87 +49,7 @@ public class ParagraphTypesetter {
 			return false;
 		}
 
-		if (typeset0(paragraph, breakStrategy, width)) {
-			buildLayoutBounds(paragraph, width);
-			return true;
-		}
-
-		return false;
-	}
-
-	private static void buildLayoutBounds(Paragraph paragraph, int width) {
-		RectF lineRect = new RectF();
-		RectF boxRect = new RectF();
-		Layout layout = paragraph.getLayout();
-		int horizontalGravity = layout.getHorizontalGravity();
-		int paddingLeft = layout.getPaddingLeft();
-		float lineSpacingExtra = (int) layout.getLineSpacingExtra();
-		lineRect.bottom = layout.getPaddingTop() - lineSpacingExtra /* 为了方便叠加spacing */;
-
-		int height = 0;
-		int lineCount = layout.getLineCount();
-		for (int i = 0; i < lineCount; ++i) {
-			Line line = layout.getLine(i);
-			height += line.getLineHeight();
-			getLineHorizontalBounds(horizontalGravity, line, lineRect, width, paddingLeft);
-			lineRect.top = lineRect.bottom + lineSpacingExtra;
-			lineRect.bottom = lineRect.top + line.getLineHeight();
-			line.setBounds(lineRect);
-			Box prev = null;
-			boxRect.set(lineRect.left, lineRect.top, lineRect.left, lineRect.bottom);
-			for (int j = 0; j < line.getCount(); ++j) {
-				Element element = line.getElement(j);
-				if (element instanceof Box) {
-					Box current = (Box) element;
-					boxRect.right = boxRect.left + current.getWidth();
-					current.setOuterBounds(boxRect);
-					current.setInnerBounds(boxRect);
-					if (prev != null) {
-						prev.linkBounds(current);
-					}
-					prev = current;
-					boxRect.left = boxRect.right;
-					continue;
-				}
-				boxRect.left += getAdjustGlueWidth(line, (Glue) element);
-			}
-			line.trim();
-		}
-
-		width = width + paddingLeft + layout.getPaddingRight();
-		height = height + layout.getPaddingTop() + layout.getPaddingBottom();
-		if (lineCount != 0) {
-			height += (int) Math.ceil((lineSpacingExtra * (lineCount - 1)));
-		}
-		layout.setContentSize(width, height);
-	}
-
-	private static float getAdjustGlueWidth(Line line, Glue glue) {
-		float ratio = line.getRatio();
-		if (ratio == 0) {
-			return glue.getWidth();
-		}
-
-		if (ratio > 0) {
-			return glue.getWidth() + ratio * glue.getStretch();
-		}
-
-		return glue.getWidth() + ratio * glue.getShrink();
-	}
-
-	private static void getLineHorizontalBounds(int horizontalGravity, Line line, RectF bounds, int width, int paddingLeft) {
-		if (horizontalGravity == TextGravity.START) {
-			bounds.left = paddingLeft;
-		} else if (horizontalGravity == TextGravity.CENTER_HORIZONTAL) {
-			float offsetX = (width - line.getLineWidth()) / 2.0f;
-			bounds.left = paddingLeft + offsetX;
-		} else if (horizontalGravity == TextGravity.END) {
-			float offsetX = width - line.getLineWidth();
-			bounds.left = paddingLeft + offsetX;
-		} else {
-			throw new IllegalStateException("unknown text gravity");
-		}
-		bounds.right = bounds.left + line.getLineWidth();
+		return typeset0(paragraph, breakStrategy, width, false);
 	}
 
 	/**
@@ -138,31 +58,20 @@ public class ParagraphTypesetter {
 	 * @return 排版是否成功
 	 */
 	public boolean desire(Paragraph paragraph, BreakStrategy breakStrategy) {
-		if (!typeset0(paragraph, breakStrategy, AbsParagraphTypesetter.INFINITY_WIDTH)) {
-			return false;
-		}
-
-		Layout layout = paragraph.getLayout();
-		float actualWidth = 0;
-		for (int i = 0; i < layout.getLineCount(); ++i) {
-			Line line = layout.getLine(i);
-			actualWidth = Math.max(line.getLineWidth(), actualWidth);
-		}
-		buildLayoutBounds(paragraph, (int) Math.ceil(actualWidth));
-		return true;
+		return typeset0(paragraph, breakStrategy, AbsParagraphTypesetter.INFINITY_WIDTH, true);
 	}
 
-	private boolean typeset0(Paragraph paragraph, BreakStrategy breakStrategy, int width) {
+	private boolean typeset0(Paragraph paragraph, BreakStrategy breakStrategy, int width, boolean desire) {
 		if (DEBUG) {
 			++mStatus.mCount;
 			mStatus.mInternalState = null;
 		}
 
 		if (breakStrategy == BreakStrategy.SIMPLE) {
-			return mSimpleTypesetter.typeset(paragraph, breakStrategy, width);
+			return mSimpleTypesetter.typeset(paragraph, breakStrategy, width, desire);
 		}
 
-		if (!mTexTypesetter.typeset(paragraph, breakStrategy, width)) {
+		if (!mTexTypesetter.typeset(paragraph, breakStrategy, width, desire)) {
 			// tex 存在找不到完美解的情况，如果在这种case下
 			// 回归到朴素的排版算法
 			if (DEBUG) {
@@ -170,7 +79,7 @@ public class ParagraphTypesetter {
 				Log.w("ParagraphTypesetter", "can not find active nodes: " + paragraph);
 			}
 
-			return mSimpleTypesetter.typeset(paragraph, breakStrategy, width);
+			return mSimpleTypesetter.typeset(paragraph, breakStrategy, width, desire);
 		}
 
 		if (DEBUG) {
