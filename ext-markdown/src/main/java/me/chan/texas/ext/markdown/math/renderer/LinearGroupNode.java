007bb62eda112b47f58fdc41c2e2620aa686c717
ChanJLee <wuliyichen@gmail.com>
2025-10-19 21:47:54 +0800

    support linear layout group node

---
+++ ext-markdown/src/main/java/me/chan/texas/ext/markdown/math/renderer/LinearGroupNode.java
@@ -0,0 +1,79 @@
+package me.chan.texas.ext.markdown.math.renderer;
+
+import java.util.List;
+
+import me.chan.texas.renderer.core.graphics.TexasCanvas;
+import me.chan.texas.renderer.core.graphics.TexasPaint;
+
+public class LinearGroupNode extends RendererNode {
+	private final List<RendererNode> mNodes;
+	private final Gravity mGravity;
+
+	public LinearGroupNode(float scale, List<RendererNode> nodes, Gravity gravity) {
+		super(scale);
+		mNodes = nodes;
+		mGravity = gravity;
+	}
+
+	@Override
+	protected void onMeasure(TexasPaint paint) {
+		for (RendererNode node : mNodes) {
+			node.measure(paint);
+		}
+	}
+
+	@Override
+	protected void onLayoutChildren() {
+		if (mGravity == Gravity.HORIZONTAL) {
+			layoutHorizontal();
+			return;
+		}
+
+		layoutVertical();
+	}
+
+	private void layoutHorizontal() {
+		float left = 0;
+		float bottom = 0;
+		for (RendererNode node : mNodes) {
+			node.layout(left, 0);
+			left = node.getRight();
+			bottom = Math.max(bottom, node.getBottom());
+		}
+
+		for (RendererNode node : mNodes) {
+			node.translate(0, (bottom - node.getHeight()) / 2);
+		}
+	}
+
+	private void layoutVertical() {
+		float top = 0;
+		float right = 0;
+		for (RendererNode node : mNodes) {
+			node.layout(0, top);
+			top = node.getBottom();
+			right = Math.max(right, node.getRight());
+		}
+
+		for (RendererNode node : mNodes) {
+			node.translate((right - node.getWidth()) / 2, 0);
+		}
+	}
+
+	@Override
+	protected void onDraw(TexasCanvas canvas, TexasPaint paint) {
+		for (RendererNode node : mNodes) {
+			node.draw(canvas, paint);
+		}
+	}
+
+	@Override
+	protected String toPretty() {
+		return "[]";
+	}
+
+	public enum Gravity {
+		HORIZONTAL,
+		VERTICAL,
+	}
+}
