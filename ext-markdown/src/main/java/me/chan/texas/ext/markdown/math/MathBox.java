3f6ad655555f965019c2e561249bcbf704c0a7f4
chan <chanlee@MacBook-Air-6.local>
2025-12-04 15:11:34 +0800

    support math box

---
+++ ext-markdown/src/main/java/me/chan/texas/ext/markdown/math/MathBox.java
@@ -0,0 +1,20 @@
+package me.chan.texas.ext.markdown.math;
+
+import me.chan.texas.misc.RectF;
+import me.chan.texas.renderer.core.graphics.TexasCanvas;
+import me.chan.texas.renderer.core.graphics.TexasPaint;
+import me.chan.texas.text.HyperSpan;
+import me.chan.texas.text.layout.StateList;
+
+public class MathBox extends HyperSpan {
+
+	@Override
+	protected void onDraw(TexasCanvas canvas, TexasPaint paint, RectF inner, RectF outer, float baselineOffset, StateList states) {
+
+	}
+
+	@Override
+	protected void onMeasure(float lineHeight, float baselineOffset) {
+
+	}
+}
