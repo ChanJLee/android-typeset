4b4927bc9cb4e61f897a056db64e310907ffde28
chan <chanlee@MacBook-Air-6.local>
2025-12-15 15:40:56 +0800

    fix dot symbol render

--- ext-markdown/src/main/java/me/chan/texas/ext/markdown/math/renderer/fonts/SymbolOptions.java
+++ ext-markdown/src/main/java/me/chan/texas/ext/markdown/math/renderer/fonts/SymbolOptions.java
@@ -143,9 +143,9 @@ public class SymbolOptions {
 		// dotaccent ˙
 		Sk.put("dotaccent", new Symbol("\u02D9", 85, 551, 192, 657));
 		// ddotaccent ˙
-		Sk.put("ddotaccent", new Symbol("\u02D9\u02D9", 85, 551, 299, 657));
+		Sk.put("ddotaccent", new Symbol("\u02D9\u02D9", 85, 551, 468, 657));
 		// dddotaccent ˙
-		Sk.put("dddotaccent", new Symbol("\u02D9\u02D9\u02D9", 85, 551, 406, 657));
+		Sk.put("dddotaccent", new Symbol("\u02D9\u02D9\u02D9", 85, 551, 758, 657));
 		// ring ˚
 		Sk.put("ring", new Symbol("\u02DA", 279, 529, 471, 705));
 		// ogonek ˛
