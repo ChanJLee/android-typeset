c3e281db94d6faa087286ccc41c289e91448b6d0
chan <chanlee@MacBook-Air-6.local>
2025-12-30 10:45:47 +0800

    support tracker

--- ext-markdown-math/src/main/java/me/chan/texas/ext/markdown/math/ast/MathParser.java
+++ ext-markdown-math/src/main/java/me/chan/texas/ext/markdown/math/ast/MathParser.java
@@ -87,6 +87,7 @@ public class MathParser {
 			{"[", "]"},
 			{"{", "}"},
 			{"|", "|"},
+			{"||", "||"},
 			{"\\|", "\\|"},
 			{"\\langle", "\\rangle"},
 			{"\\lfloor", "\\rfloor"},
--- ext-markdown-math/src/test/java/me/chan/texas/ext/markdown/math/renderer/MathInflaterUnitTest.kt
+++ ext-markdown-math/src/test/java/me/chan/texas/ext/markdown/math/renderer/MathInflaterUnitTest.kt
@@ -972,19 +972,8 @@ class MathInflaterUnitTest {
     }
 
     @Test
-    fun `test left right with pipes 1`() {
+    fun `test left right with pipes`() {
         inflate("\\left\\|x\\right\\|") {
-            brace {
-                left { stretchy { symbol("∥") } }
-                content { text { content("x") } }
-                right { stretchy { symbol("∥") } }
-            }
-        }
-    }
-
-    @Test
-    fun `test left right with pipes 2`() {
-        inflate("\\left||x\\right||") {
             brace {
                 left { stretchy { symbol("∥") } }
                 content { text { content("x") } }
