a4e2acfb7ef32cc720a90f1d3e90d97ca7ffb124
chan <chanlee@MacBook-Air-6.local>
2026-02-03 17:02:29 +0800

    support index of segment

--- library/src/main/java/me/chan/texas/renderer/ui/RendererAdapterImpl.java
+++ library/src/main/java/me/chan/texas/renderer/ui/RendererAdapterImpl.java
@@ -393,6 +393,14 @@ public class RendererAdapterImpl extends RecyclerView.Adapter<RendererAdapterImp
 			return -1;
 		}
 
+		if (segment instanceof Paragraph) {
+			Paragraph paragraph = (Paragraph) segment;
+			SelectableSegment outer = (SelectableSegment) paragraph.getTag(R.id.me_chan_texas_paragraph_selection_tag);
+			if (outer != null) {
+				segment = (Segment) outer;
+			}
+		}
+
 		return mDocument.indexOfSegment(segment);
 	}
 
