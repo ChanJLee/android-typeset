75ab1da19bb16f4500e82f1ea51fc0fdd5a8e865
chan <chanlee@MacBook-Air-6.local>
2026-03-16 17:31:06 +0800

    support texas segment animator

--- library/src/main/java/me/chan/texas/renderer/ui/rv/anim/SegmentAnimator.java
+++
@@ -1,8 +0,0 @@
-package me.chan.texas.renderer.ui.rv.anim;
-
-import android.animation.Animator;
-import android.view.View;
-
-import androidx.annotation.Nullable;
-
-import me.chan.texas.text.Segment;
